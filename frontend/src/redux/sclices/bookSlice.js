4ade6f8fb8c55390c28c4dbe733f663ff574e4c0
Alena Petrych <abelokonskaya.87@gmail.com>
2025-10-31 14:25:55 +0000

    feat: add thunkFunction

--- frontend/src/redux/sclices/bookSlice.js
+++ frontend/src/redux/sclices/bookSlice.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { createSlice } from "@reduxjs/toolkit";
+import createBookWithID from "../../utils/createBookWithID.js";
 
 const initialState = [];
 
@@ -30,6 +32,17 @@ const booksSlice = createSlice({
 
 // action types
 export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
+
+export const thunkFunction = async (dispatch, getState) => {
+  try {
+    const res = await axios.get("http://localhost:4000/random-book");
+    if (res?.data?.title && res?.data?.author) {
+      dispatch(addBook(createBookWithID(res.data, "API")));
+    }
+  } catch (error) {
+    console.log("Error fetching random book", error);
+  }
+};
 
 // создали селектор
 export const selectBooks = (state) => state.books;
